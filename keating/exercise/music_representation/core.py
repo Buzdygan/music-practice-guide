324fbdda94625cb185394fb5b4eac01573339da4
jakub <tlalka.jakub@gmail.com>
2022-12-11 12:46:10 +0100

    Add core musical structures

---
+++ keating/exercise/music_representation/core.py
@@ -0,0 +1,38 @@
+""" Core musical structures"""
+
+from typing import Optional, Tuple, NamedTuple
+from enum import Enum
+
+
+Fraction = Tuple[int, int]
+Pitch = int
+RelativePitch = int
+
+
+class Mode(Enum):
+    MAJOR = "major"
+    MINOR = "minor"
+
+
+class Modifier(Enum):
+    """Defines note modifiers"""
+
+
+class _Key(NamedTuple):
+    center: Pitch
+    mode: Mode
+
+
+class Key(Enum):
+    C = _Key(center=0, mode=Mode.MAJOR)
+
+
+class Spacement(NamedTuple):
+    position: Fraction
+    duration: Fraction
+
+
+class Note(NamedTuple):
+    relative_pitch: RelativePitch
+    spacement: Spacement
+    modifiers: Optional[Tuple[Modifier]] = None
